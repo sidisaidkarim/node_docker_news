cb7b1f732305fa3ba7b3a5a6fea6acd17b29967f
sisisaidkarim <sskarim85@gmail.com>
2019-05-31 17:49:27 +0100

    added article creation

--- init.sql
+++ init.sql
@@ -36,51 +36,51 @@ PRIMARY KEY (id)
 
 
 
-insert into news.article (title,body,create_at,updated_at)
-values ('Javascript','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-It has survived not only five centuries, but also the leap into electronic typesetting, 
-remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-containing Lorem Ipsum passages, and more recently with desktop
-publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now()),
-('Python','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-It has survived not only five centuries, but also the leap into electronic typesetting, 
-remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-containing Lorem Ipsum passages, and more recently with desktop
-publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now()),
-('Java','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-It has survived not only five centuries, but also the leap into electronic typesetting, 
-remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-containing Lorem Ipsum passages, and more recently with desktop
-publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now()),
-('HTML/CSS','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-It has survived not only five centuries, but also the leap into electronic typesetting, 
-remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-containing Lorem Ipsum passages, and more recently with desktop
-publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now()),
-('C#/C++','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-It has survived not only five centuries, but also the leap into electronic typesetting, 
-remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-containing Lorem Ipsum passages, and more recently with desktop
-publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now());
+-- insert into news.article (title,body,create_at,updated_at)
+-- values ('Javascript','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+-- Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+-- when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+-- It has survived not only five centuries, but also the leap into electronic typesetting, 
+-- remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+-- containing Lorem Ipsum passages, and more recently with desktop
+-- publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now()),
+-- ('Python','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+-- Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+-- when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+-- It has survived not only five centuries, but also the leap into electronic typesetting, 
+-- remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+-- containing Lorem Ipsum passages, and more recently with desktop
+-- publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now()),
+-- ('Java','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+-- Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+-- when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+-- It has survived not only five centuries, but also the leap into electronic typesetting, 
+-- remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+-- containing Lorem Ipsum passages, and more recently with desktop
+-- publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now()),
+-- ('HTML/CSS','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+-- Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+-- when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+-- It has survived not only five centuries, but also the leap into electronic typesetting, 
+-- remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+-- containing Lorem Ipsum passages, and more recently with desktop
+-- publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now()),
+-- ('C#/C++','Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+-- Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+-- when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+-- It has survived not only five centuries, but also the leap into electronic typesetting, 
+-- remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+-- containing Lorem Ipsum passages, and more recently with desktop
+-- publishing software like Aldus PageMaker including versions of Lorem Ipsum',now(),now());
 
-update news.article set image = 'http://www.purelogics.net/blog/wp-content/uploads/2019/01/javascript.png'
-where title = 'Javascript';
-update news.article set image = 'https://babulnokrek.com/wp-content/uploads/2018/11/Python.png'
-where title = 'Python';
-update news.article set image = 'https://images.techhive.com/images/article/2013/02/oracle_java-100026145-orig.jpg'
-where title = 'Java';
-update news.article set image = 'https://www.seoclerk.com/pics/514545-1UKNne1487866218.jpg'
-where title = 'HTML/CSS';
+-- update news.article set image = 'http://www.purelogics.net/blog/wp-content/uploads/2019/01/javascript.png'
+-- where title = 'Javascript';
+-- update news.article set image = 'https://babulnokrek.com/wp-content/uploads/2018/11/Python.png'
+-- where title = 'Python';
+-- update news.article set image = 'https://images.techhive.com/images/article/2013/02/oracle_java-100026145-orig.jpg'
+-- where title = 'Java';
+-- update news.article set image = 'https://www.seoclerk.com/pics/514545-1UKNne1487866218.jpg'
+-- where title = 'HTML/CSS';
 
-update news.article set image = 'https://hackr.io/blog/wp-content/uploads/2018/07/c-uses-and-applications@2x-1280x800.png'
-where title = 'C#/C++';
+-- update news.article set image = 'https://hackr.io/blog/wp-content/uploads/2018/07/c-uses-and-applications@2x-1280x800.png'
+-- where title = 'C#/C++';
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require("express-handlebars");
+const bodyParser = require('body-parser')
 
 const con = require("./db/connect")
 // Constants
@@ -13,15 +14,16 @@ const hbs = exphbs.create({
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 // App
 
 app.get('/', (req, res) => {
 
-  const q = `select * from article`;
+  const q = `select * from article order by create_at limit 8`;
   con.query(q,(err,result)=>{
       if (err) throw err;
-      
+      result.map(item=>item.body = item.body.substring(0,120)+'...')
       return res.render("home",{
           articles: result,
       })
@@ -29,9 +31,39 @@ app.get('/', (req, res) => {
 
 });
 
-// app.get('/article/:id',(req,res)=>{
+app.get('/article/:id',(req,res)=>{
+
+  const articleId = req.params.id;
+  const q = `select * from article where id = ${articleId}`
+  con.query(q,(err,result)=>{
+      if (err) throw err;
+       
+      return res.render("article-detail",{
+        article: result[0]
+      })
+  })
     
-// })
+})
+
+app.get("/add-new-article",(req,res)=>{
+  res.render("add-new-article");
+})
+
+app.post("/add-new-article",(req,res)=>{
+
+
+  console.log(req.body.articleTitle)
+  const title = req.body.articleTitle;
+  const body = req.body.articleBody;
+  const link = req.body.articleImgLink;
+
+  const q = `insert into article (title,body,image) value('${title}','${body}','${link}')`;
+
+  con.query(q, (err,result)=>{
+    return res.render('home');
+  } )
+
+})
 
 
 app.listen(PORT, HOST);
